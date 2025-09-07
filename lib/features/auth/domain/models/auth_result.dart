24b874439a3704981897c3ff6c4c9619192e09f3
david98ab <davidgab08@gmail.com>
2025-09-07 03:10:15 +0200

    feat: implement User Profile

--- lib/features/auth/domain/models/auth_result.dart
+++
@@ -1,7 +0,0 @@
-import 'package:multitec_app/features/user/domain/models/user.dart';
-
-class AuthResult {
-  const AuthResult({required this.user, required this.token});
-  final User user;
-  final String token;
-}
