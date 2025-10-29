dc8abb12ba7c07189729255085df54a6dc2a0e47
vcv8 <vcv183@gmail.com>
2025-10-29 18:17:58 +0100

    fix: :ambulance: Fix ios build. Update minimum ios deployment target

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,20 +9,6 @@ import GoogleMaps
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GeneratedPluginRegistrant.register(with: self)
-        
-        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-        let channel = FlutterMethodChannel(name: ApiConstants.apiKeyChannelName, binaryMessenger: controller.binaryMessenger)
-
-        channel.setMethodCallHandler { (call: FlutterMethodCall, result: FlutterResult) in
-            if call.method == ApiConstants.setGoogleMapsApiKeyMethod {
-                if let apiKey = call.arguments as? String {
-                    GMSServices.provideAPIKey(apiKey)
-                }
-                result(nil)
-            } else {
-                result(FlutterMethodNotImplemented)
-            }
-        }
 
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
